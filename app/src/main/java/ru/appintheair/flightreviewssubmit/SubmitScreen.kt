40f2d8341bbe7553dd1a4be87b8551b466574797
Punk_Off <pankov.kirill1997@yandex.ru>
2020-11-02 12:15:05 +0300

    Implementation Coordinator layout

--- app/src/main/java/ru/appintheair/flightreviewssubmit/SubmitScreen.kt
+++ app/src/main/java/ru/appintheair/flightreviewssubmit/SubmitScreen.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 
 class SubmitScreen : Fragment() {
     private var mSubmitScreenViewModel: SubmitScreenViewModel? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
